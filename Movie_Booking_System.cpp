1011494018aa5b09abdc7fbf72f18309185825fd
Mohamed Hafez <hafezm988@gmail.com>
2023-07-06 00:34:38 +0300

    Add project files.

---
+++ Movie_Booking_System.cpp
@@ -0,0 +1,68 @@
+/*
+					KEEP GOING
+*/
+#include <iostream>
+#include <sstream>
+#include <windows.h>
+#include <math.h>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <vector>
+
+// My Classes
+#include "Movie.h"
+#include "Seat.h"
+#include "Rating.h"
+#include "User.h"
+#include "Booking.h"
+
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define start int main()
+#define infinite while(true)
+#define space "\t\t"
+#define elif else if
+#define br '\n'
+#define bye return 0
+#define ll long long
+#define ld long double
+#define dl double
+#define fl float
+#define str string
+#define red system("color 04")
+#define green system("color 02")
+#define cls system("cls")
+const float pi = acos(-1);
+using namespace std;
+void ART() {
+    cout << "                                    *         *      *         *\n";
+    cout << "                                ***          **********          ***\n";
+    cout << "                             *****           **********           *****\n";
+    cout << "                           *******           **********           *******\n";
+    cout << "                         **********         ************         **********\n";
+    cout << "                        ****************************************************\n";
+    cout << "                       ******************************************************\n";
+    cout << "                      ********************************************************\n";
+    cout << "                      ********************************************************\n";
+    cout << "                      ********************************************************\n";
+    cout << "                       ******************************************************\n";
+    cout << "                        ********      ************************      ********\n";
+    cout << "                         *******       *     *********      *       *******\n";
+    cout << "                           ******             *******              ******\n";
+    cout << "                             *****             *****              *****\n";
+    cout << "                                ***             ***              ***\n";
+    cout << "                                  **             *              **\n";
+    Sleep(3000);
+    cls;
+}
+start
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    green;
+	ART();
+	bye;
+}
